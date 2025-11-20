0e713967ea1de2c30c1af5c4b6caab098ba9b1cd
stefanopaolonii <stefanopaoloni.work@gmail.com>
2025-11-20 23:11:03 +0100

    fix

--- backend/api/new-badge.js
+++ backend/api/new-badge.js
@@ -21,7 +21,6 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     
-ù
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
     }
