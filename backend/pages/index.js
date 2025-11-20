dffed789e5deee1d53e918ab78a78af0085a2094
stefanopaolonii <stefanopaoloni.work@gmail.com>
2025-11-20 22:27:45 +0100

    redirect update

--- backend/pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return null;
-}
-
-export async function getServerSideProps({ res }) {
-  res.writeHead(302, { Location: 'https://stefanopaolonii.github.io/visitor-badge/' });
-  res.end();
-  return { props: {} };
-}
