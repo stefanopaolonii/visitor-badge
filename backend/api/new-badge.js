251da34bca095cc8a36ffbcb9beb992717febb7b
stefanopaolonii <stefanopaoloni.work@gmail.com>
2025-11-20 23:16:40 +0100

    update

--- backend/api/new-badge.js
+++ backend/api/new-badge.js
@@ -17,13 +17,13 @@ async function generateNewKey() {
 }
 
 export default async function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    
-    if (req.method === 'OPTIONS') {
-        return res.status(200).end();
-    }
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
     try {
         if (req.method !== "POST") {
             return res.status(405).json({ error: "Method Not Allowed" });
